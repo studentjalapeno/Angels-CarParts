801d6e30c5a69b63dc8ae822b84ac852690f7c60
studentjalapeno <60950099+studentjalapeno@users.noreply.github.com>
2021-08-02 17:19:04 -0500

    started products components

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+const products = [
+    { id: 1, name: 'Engine', description: 'Honda Engine'},
+    { id: 1, name: 'Spark Plugs', description: 'Bosch lridium'},
+];
+
+
+const Products = () => {
+
+    // noinspection BadExpressionStatementJS
+    <main>
+
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) =>  (
+               <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+               </Grid>
+         ))}
+        </Grid>
+    </main>
+};
+
+export default Products;
