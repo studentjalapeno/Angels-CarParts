e1e354ca870c1a955f6c216729aa18854e5eeb60
studentjalapeno <60950099+studentjalapeno@users.noreply.github.com>
2021-08-06 13:21:49 -0500

    finished product component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import {Grid} from '@material-ui/core';
+
+import Product from "./Product/Product";
 
 const products = [
-    { id: 1, name: 'Engine', description: 'Honda Engine'},
-    { id: 1, name: 'Spark Plugs', description: 'Bosch lridium'},
+    { id: 1, name: 'Engine', description: 'Honda Engine', price: "$1", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-chevrolet-corvette-lt2-engine-001-1582310258.jpg"},
+    { id: 1, name: 'Spark Plugs', description: 'Bosch lridium', price: "$2", image: "https://m.media-amazon.com/images/I/71999q76ALL._AC_SL1500_.jpg"},
 ];
 
 
 const Products = () => {
 
-    // noinspection BadExpressionStatementJS
+    return(
     <main>
 
         <Grid container justify="center" spacing={4}>
             {products.map((product) =>  (
-               <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
-               </Grid>
-         ))}
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                </Grid>
+            ))}
         </Grid>
     </main>
+    );
+
 };
 
 export default Products;
