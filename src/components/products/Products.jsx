b141cef0b7aae26660615492b35b781721e469fa
studentjalapeno <60950099+studentjalapeno@users.noreply.github.com>
2021-08-20 10:43:05 -0500

    added commerce products using Commerce.js

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,13 +4,8 @@ import {Grid} from '@material-ui/core';
 import Product from "./Product/Product";
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Engine', description: 'Honda Engine', price: "$1", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-chevrolet-corvette-lt2-engine-001-1582310258.jpg"},
-    { id: 1, name: 'Spark Plugs', description: 'Bosch lridium', price: "$2", image: "https://m.media-amazon.com/images/I/71999q76ALL._AC_SL1500_.jpg"},
-];
 
-
-const Products = () => {
+const Products = ({products}) => {
     const classes = useStyles();
     return(
     <main className={classes.content}>
